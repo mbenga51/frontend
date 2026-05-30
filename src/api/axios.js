b0e95d7c817c17be1ae9468f7faa9eb61363beea
Mariama Mbenga <mm22223564@utg.edu.gm>
2026-05-30 22:44:06 +0000

    error

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Update with your backend API URL
+  baseURL: `${import.meta.env.VITE_URL_API}/api`, // Update with your backend API URL
   headers: {
     'Content-Type': 'application/json',
   },
